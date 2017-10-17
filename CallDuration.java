aa101ede919d3634829ada4b4305a98eca35b37d
allen sebastian <allensebastian89@gmail.com>
2017-10-17 15:19:06 +0530

    Add files via upload

---
+++ CallDuration.java
@@ -0,0 +1,11 @@
+package com.ventuno.result;
+
+public class CallDuration {
+
+	
+		public Integer totalCalls = 0;
+		public Integer totalCallDuration = 0;
+		public Integer maxCallDuration = Integer.MAX_VALUE;
+		public Integer minCallDuration = 0;
+		public Integer avgCallDuration = 0;
+	}
